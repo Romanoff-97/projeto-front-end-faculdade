95e49dbaf3911ade08db26b84590a3cbc21d8ac0
Romanoff-97 <143611502+nathalya-epg@users.noreply.github.com>
2025-05-06 17:04:36 -0300

    :recycle: refactor removendo console.log e adicionando um timeout

--- src/js/funcoes.js
+++ src/js/funcoes.js
@@ -11,17 +11,21 @@ export function carregarConteudo(pagina) {
     .catch((error) => console.error("Erro ao carregar conteúdo:", error));
 }
 
+/**
+ * Envia uma mensagem de contato construindo um link mailto e abrindo-o
+ * no aplicativo de email padrão do usuário. Extrai o nome, email e
+ * conteúdo da mensagem dos elementos DOM com ids "name", "email" e
+ * "message". Exibe um alerta para confirmar o envio da mensagem.
+ */
 export function enviarMensagem() {
   const nome = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const mensagem = document.getElementById("message").value;
 
-  console.log(nome, email, mensagem);
-
   const mailtoLink = `mailto:nscidreira@hotmail.com?subject=Contato%20de%20${nome}&body=Nome:%20${nome}%0AEmail:%20${email}%0AMensagem:%20${mensagem}`;
+  alert(`Ola ${nome}, obrigado por entrar em contato!`);
 
-  window.location.href = mailtoLink;
-
-  alert(`Ola ${nome}, obrigado por entrar em contato!
-  Sua mensagem foi enviada com sucesso!`);
+  setTimeout(() => {
+    window.location.href = mailtoLink;
+  }, 300);
 }
