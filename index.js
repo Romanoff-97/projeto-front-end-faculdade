0add1915e44e428bbf0b68ad8424a090c6f62f1d
Nathalya Cidreira <79860480+Romanoff-97@users.noreply.github.com>
2025-05-07 17:59:19 +0000

    :recycle: refactor ajustando todas as funcoes em um arquivo js

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-import * as funcoes from "./funcoes";
+/**
+ * Carrega o conteúdo da página especificada na div com o id "conteudo"
+ * @param {string} pagina - URL da página a ser carregada
+ */
+function carregarConteudo(pagina) {
+  fetch(pagina)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("conteudo").innerHTML = data;
+    })
+    .catch((error) => console.error("Erro ao carregar conteúdo:", error));
+}
 
-funcoes.carregarConteudo("home.html");
-window.funcoes = funcoes;
+/**
+ * Envia uma mensagem de contato construindo um link mailto e abrindo-o
+ * no aplicativo de email padrão do usuário. Extrai o nome, email e
+ * conteúdo da mensagem dos elementos DOM com ids "name", "email" e
+ * "message". Exibe um alerta para confirmar o envio da mensagem.
+ */
+function enviarMensagem() {
+  const nome = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const mensagem = document.getElementById("message").value;
+
+  if (nome == '' || email == '' || mensagem == '') {
+    alert(`Preencha todos os campos do formulário antes de continuar.`);
+  } else {
+    const mailtoLink = `mailto:nscidreira@hotmail.com?subject=Contato%20de%20${nome}&body=Nome:%20${nome}%0AEmail:%20${email}%0AMensagem:%20${mensagem}`;
+    alert(`Ola ${nome}, obrigado por entrar em contato!`);
+
+    setTimeout(() => {
+      window.location.href = mailtoLink;
+    }, 300);
+  }
+
+}
+
+
+carregarConteudo("home.html");
 
 /**
  * Adicionando evento de clique aos links da navegação
@@ -12,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
   navLinks.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
-      funcoes.carregarConteudo(link.getAttribute("href"));
+      carregarConteudo(link.getAttribute("href"));
 
       if (link.textContent === "Contato") {
         setTimeout(() => {
@@ -22,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
           const formContatoEl = document.getElementById("form-contato");
           formContatoEl.addEventListener("submit", (event) => {
             event.preventDefault();
-            funcoes.enviarMensagem();
+            enviarMensagem();
           });
         }, 600);
       }
